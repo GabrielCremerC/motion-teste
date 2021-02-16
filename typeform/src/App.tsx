2336ec78b811f42e7f2684c15e067fab98a209db
Gabriel Cremer <GabrielCremer01@gmail.com>
2021-02-16 12:08:34 -0300

    Motion Teste

---
+++ typeform/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Main, Section, Sticky } from './styles';
+import GlobalStyles from './styles/global';
+import FirstAndSecond from './components/FirstAndSecond/index';
+import Header from './components/Header';
+
+function App() {
+  React.useEffect(() =>{
+    window.scrollTo({top:0})
+  }, [])
+  return (
+      <>
+        <GlobalStyles/>
+        <Header/>
+
+        <Main style={{height: '1610vh'}}>
+          <Section style={{height: '72.7%'}} >
+            <FirstAndSecond/>
+
+          </Section>
+
+          <Section style={{height: '9.7%'}}>
+            <Sticky className="third" />
+          </Section>
+
+          <Section style={{height: '10.1%'}}>
+            <Sticky className="four" />
+          </Section>
+        </Main>
+      </>
+  );
+}
+
+export default App;
